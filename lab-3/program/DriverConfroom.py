b26fd4c4ad7fbe20018fb6c6bba802a4bc9c3d84
GarciaAlegria <abnerivan2003@gmail.com>
2024-07-17 18:32:35 -0600

    pregunta 1 y 2

--- lab-3/program/DriverConfroom.py
+++ lab-3/program/DriverConfroom.py
@@ -2,13 +2,41 @@ import sys
 from antlr4 import *
 from ConfRoomSchedulerLexer import ConfRoomSchedulerLexer
 from ConfRoomSchedulerParser import ConfRoomSchedulerParser
+from ConfRoomSchedulerListener import ConfRoomSchedulerListener
+
+class ConfRoomScheduler(ConfRoomSchedulerListener):
+    def __init__(self):
+        self.reservations = {}
+
+    def enterReserveStat(self, ctx: ConfRoomSchedulerParser.ReserveStatContext):
+        id = ctx.reserve().ID().getText()
+        date = ctx.reserve().DATE().getText()
+        start_time = ctx.reserve().TIME(0).getText()
+        end_time = ctx.reserve().TIME(1).getText()
+        self.reservations[(id, date, start_time, end_time)] = 'RESERVED'
+        print(f"Reserved: {id} on {date} from {start_time} to {end_time}")
+
+    def enterCancelStat(self, ctx: ConfRoomSchedulerParser.CancelStatContext):
+        id = ctx.cancel().ID().getText()
+        date = ctx.cancel().DATE().getText()
+        start_time = ctx.cancel().TIME(0).getText()
+        end_time = ctx.cancel().TIME(1).getText()
+        if (id, date, start_time, end_time) in self.reservations:
+            del self.reservations[(id, date, start_time, end_time)]
+            print(f"Cancelled: {id} on {date} from {start_time} to {end_time}")
+        else:
+            print(f"No reservation found to cancel: {id} on {date} from {start_time} to {end_time}")
 
 def main(argv):
     input_stream = FileStream(argv[1])
     lexer = ConfRoomSchedulerLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = ConfRoomSchedulerParser(stream)
-    tree = parser.prog()  # We are using 'prog' since this is the starting rule based on our ConfRoomScheduler grammar, yay!
+    tree = parser.prog()
+
+    scheduler = ConfRoomScheduler()
+    walker = ParseTreeWalker()
+    walker.walk(scheduler, tree)
 
 if __name__ == '__main__':
     main(sys.argv)
